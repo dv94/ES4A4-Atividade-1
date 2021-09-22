bdeb046c4f0ce5cc72f22d4d1050f8793906f9e3
Lorena <looohmorena@gmail.com>
2021-09-22 20:48:50 -0300

    implementando os operadores (soma e subtração)

--- Atividade 1.cpp
+++ Atividade 1.cpp
@@ -22,12 +22,36 @@ int main(void){
 	
 		switch (escolha){
 		
-		case 1: 
-		
-				
+			case 1: 
+			
+			printf("\n OPÇÃO ESCOLHIDA: SOMA \n\n");
+			
+			printf(" Escolha um número: ");
+			scanf("%d", &a);
+			
+			printf("\n Escolha outro número: ");
+			scanf("%d", &b);
+			
+			soma = a + b; 
+			
+			printf("\n A soma de %d + %d é: %d ", a, b, soma); 
+			
 			break; 
 		
 			case 2: 
+			
+			printf("\n OPÇÃO ESCOLHIDA: SUBTRAÇÃO \n\n");
+			
+			printf(" Escolha um número: ");
+			scanf("%d", &a);
+			
+			printf("\n Escolha outro número: ");
+			scanf("%d", &b);
+			
+			subtracao = a - b; 
+			
+			printf("\n A soma de %d - %d é: %d ", a, b, subtracao); 
+			
 				
 			break; 
 		
@@ -39,12 +63,12 @@ int main(void){
 			break;
 		
 			default: 
-			printf(" Erro: Valor Inválido! Digite somente valores de 1 a 4\n"); 
+			printf(" Error: Valor Inválido! Digite somente valores de 1 a 4\n"); 
 			break;
 		
 		}
 		
-		printf("\n\n Digite ( 1 ) para continuar ou ( 2 ) para sair\n"); 
+		printf("\n\n Digite ( 1 ) para continuar ou ( 2 ) para sair!: "); 
 		scanf("%d", &resp); 
 		
 	}	while(resp == 1);
